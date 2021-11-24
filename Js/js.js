3c7697fd48ca654c2766a383397e353a32a353cb
felipe <felipesdcc@gmail.com>
2021-11-24 01:14:08 -0300

    Projeto 24/11

--- Js/js.js
+++ Js/js.js
@@ -1,6 +1,18 @@
+
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+
+  $(document).ready(function(){
+    $('.pushpin').pushpin();
+  });
+
+
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
   $(document).ready(function(){
     $('.tabs').tabs();
   });

8006299c459284fe0483e185f63d6228d90198cf
felipe <felipesdcc@gmail.com>
2021-11-25 11:43:22 -0300

    projetinho

--- Js/js.js
+++ Js/js.js
@@ -16,3 +16,15 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.tabs').tabs();
   });
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
+
+  var instance = M.Slider.getInstance(elem);
+
+    
+
+    $('.slider').slider('methodName');
+     $('.slider').slider('methodName', paramName);
+  
